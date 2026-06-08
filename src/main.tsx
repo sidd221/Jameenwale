f72c13aa7275a2343a8c69388d5b1440e76767db
Siddhant Sinha <99606311+sidd221@users.noreply.github.com>
2026-06-08 16:39:55 +0530

    feat: setup react-gtm-module in React app root

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import TagManager from 'react-gtm-module';
 import App from './App.tsx';
 import './index.css';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-5C7V63LJ',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
